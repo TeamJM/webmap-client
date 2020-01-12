7e9ea88aa4bb1157c1e42588396c9deb5722f911
Gareth Coles <gdude2002@gmail.com>
2020-01-12 19:16:59 +0000

    We'll stick with manual extraction for now

--- src/main/java/info/journeymap/webmap_client/Main.java
+++
@@ -1,9 +0,0 @@
-package info.journeymap.webmap_client;
-
-public class Main
-{
-    public static void main(String... args)
-    {
-        System.out.println("This isn't a runnable JAR!");
-    }
-}
