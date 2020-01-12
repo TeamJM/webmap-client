ca3dd817875736dffe7e16edc49ce628339d0f4c
Gareth Coles <gdude2002@gmail.com>
2020-01-12 17:46:47 +0000

    Attempt to fix non-shading of JAR by adding Main class

---
+++ src/main/java/info/journeymap/webmap_client/Main.java
@@ -0,0 +1,9 @@
+package info.journeymap.webmap_client;
+
+public class Main
+{
+    public static void main(String... args)
+    {
+        System.out.println("This isn't a runnable JAR!");
+    }
+}
