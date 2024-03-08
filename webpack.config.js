1b1a93887075158e7f4525e076894e56b480afe4
Mysticdrew <drewhaas@gmail.com>
2024-03-08 13:45:06 -0600

    use old css minimizer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const OptimizeCSSAssetsPlugin = require("css-minimizer-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 const TerserJSPlugin = require("terser-webpack-plugin")
 const VueLoaderPlugin = require("vue-loader/lib/plugin")
 
